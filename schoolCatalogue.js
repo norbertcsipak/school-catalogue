d78368dd3cf08e34553b9aeac992de8c781a1f58
Norbs <97234527+norbertcsipak@users.noreply.github.com>
2022-10-31 11:00:31 +0000

    created a class HighSchool that inherited everything from a parent class School with and extra parametar called sportsTeams. Created an instance of a high school and logged the values of sports teams

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -48,3 +48,20 @@ class PrimarySchool extends School {
 
 const primarySchool1 = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
 primarySchool1.quickFacts();
+
+class HighSchool extends School {
+  constructor (name, numberOfStudents, sportsTeams) {
+    super(name, 'high', numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+
+  get sportsTeams () {
+    return this._sportsTeams;
+  }
+}
+
+const highSchool1 = new HighSchool('Al E. Smith', 415,['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+console.log(highSchool1.sportsTeams);
+
+// list of all substitute teachers
+const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
